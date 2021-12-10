f1bde29131ec903bdca16d80dd6b998b8e52282b
Nyakea <shalyn.nyakea@student.moringaschool.com>
2021-12-10 17:48:03 +0300

    add approach plan(BDD).

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// prompts user to enter birth date dd/mm/yy format
+
+
+// function to calculate day of birth
+
+
+// asks for gender of user
+
+
+// confirms gender and birth date
+
+
+// assigns name to day of the week and gender
+
+
+// 
