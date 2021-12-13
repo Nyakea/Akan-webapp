9d870d8b2d8607abf88bd46e77a9202d5049f829
Nyakea <shalyn.nyakea@student.moringaschool.com>
2021-12-13 23:27:40 +0300

    collect data from form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+// Collect data
+let gender = document.getElementsByName("gender").value;
+let date = document.getElementById("date").value;
+let month = document.getElementById("month").value;
+let year = document.getElementById("year").value;
+
 // prompts user to enter birth date dd/mm/yy format
 
 
@@ -11,6 +17,7 @@
 
 
 // assigns name to day of the week and gender
+
 
 
 // 
